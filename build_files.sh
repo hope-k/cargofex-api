30f8b53b86e20e15faa5c37a259bf3d9e5e02665
hope-k <hopekumordzie@gmail.com>
2024-03-30 21:11:01 +0000

    vercel.jsonv8

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,2 @@
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --no-input --clear
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --no-input 
