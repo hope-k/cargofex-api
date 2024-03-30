61c623bd2bf4ed491f9d9dc646d347a53fe9f541
hope-k <hopekumordzie@gmail.com>
2024-03-30 19:22:57 +0000

    py versionv3.9

--- build files.sh
+++
@@ -1,9 +0,0 @@
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
-python3.9 manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python3.9 manage.py migrate
