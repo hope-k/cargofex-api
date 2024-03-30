7840169b3486f72d5dd9b4a00aa72f4a03b5492c
hope-k <hopekumordzie@gmail.com>
2024-03-30 21:28:07 +0000

    rm vercel.jsonv4

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-python -m pip install -r requirements.txt
+# Modify this line as needed for your package manager (pip, poetry, etc.
+pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
