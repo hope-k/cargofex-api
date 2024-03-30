e07895261eaf92fcfa87eddcbbdc86761d94299c
hope-k <hopekumordzie@gmail.com>
2024-03-30 15:20:22 +0000

    moved static folder

--- courier/settings/base.py
+++ courier/settings/base.py
@@ -131,7 +131,7 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / "static"
 MEDIA_ROOT = BASE_DIR / "media"
-STATIC_URL = BASE_DIR / "static/"
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
