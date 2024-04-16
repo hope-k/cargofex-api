eecb45a14a2c9bcaebc00323f8b797764df836ca
hope-k <hopekumordzie@gmail.com>
2024-04-16 21:27:11 +0000

    vercel.json addedv4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 # Convert static asset files
 python3.9 manage.py collectstatic --no-input
