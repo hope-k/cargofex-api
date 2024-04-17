489679e48e6a885d8ba933ef7c27c63b96cba90c
hope-k <hopekumordzie@gmail.com>
2024-04-17 03:51:16 +0000

    rm runtime.txtv8

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-# Build the project
-echo "Building the project..."
-
-echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
